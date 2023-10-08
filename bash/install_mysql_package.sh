288a9c4019fa37d8c55750bd58e63b50fba3158c
Alexis <alexisgraff123@gmail.com>
2023-10-08 16:02:40 -0300

    feat/ag/sql installation

--- bash/install_mysql_package.sh
+++ bash/install_mysql_package.sh
@@ -1,9 +1,29 @@
+#!/bin/bash
+
+# Actualiza la lista de paquetes
 sudo apt-get update
-sudo apt-get install mysql-server
+
+# Instala el servidor MySQL
+yes | sudo apt-get install mysql-server
+
+# Verifica si MySQL está activo
 systemctl is-active mysql
-sudo mysql_secure_installation
+
+# Configura MySQL de forma segura
+sudo mysql_secure_installation <<EOF
+Y
+123
+Y
+Y
+Y
+Y
+EOF
+
+# Cambia la autenticación de root a mysql_native_password
 sudo mysql <<EOF
 ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY '123';
 FLUSH PRIVILEGES;
 EOF
+
+# Instala MySQL Workbench
 sudo snap install mysql-workbench-community
